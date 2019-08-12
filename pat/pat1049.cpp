ecab478dd791fa0aafc6cc7c27a46f498cf0f657
Laizl95 <641449078@qq.com>
2019-08-12 22:09:06 +0800

    提交

--- pat/pat1049.cpp
+++ pat/pat1049.cpp
@@ -1,16 +1,37 @@
-#include<bits/stdc++.h>
-#define rep(i,x,y) for(int i=x;i<y;++i)
-#define per(i,x,y) for(int i=y-1;i>=x;--i)
-#define ms(x,y) memset(x,y,sizeof(x))
-#define pb(x) push_back(x)
+#include <bits/stdc++.h>
 using namespace std;
-const int N=100005;
-
-int main(){
+int num[12],dp[12][12];
+int dfs(int pos,int now,int limit)
+{
+    if(pos==-1)
+        return now;
+    int &ans=dp[pos][now];
+    if(!limit&&ans!=-1)
+        return ans;
+    ans=0;
+    int up=limit?num[pos]:9;
+    for(int i=0; i<=up; i++)
+    {
+        ans+=dfs(pos-1,i==1?now+1:now,limit&&i==up);
+    }
+    return ans;
+}
+int solve(int x)
+{
+    int cnt=0;
+    while(x)
+    {
+        num[cnt++]=x%10;
+        x/=10;
+    }
+    return dfs(cnt-1,0,1);
+}
+int main()
+{
     int n;
     cout<<(1<<30)<<endl;
-    while(scanf("%d",&n)!=EOF){
-
-    }
-return 0;
+    scanf("%d",&n);
+    memset(dp,-1,sizeof(dp));
+    printf("%d\n",solve(n));
+    return 0;
 }
